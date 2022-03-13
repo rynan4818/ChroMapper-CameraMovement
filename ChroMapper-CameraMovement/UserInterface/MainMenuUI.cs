6b69a28b5c136d756ea2ff3ad3df8b35dcb7c7d6
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2022-03-13 13:05:12 +0900

    NJS Editor Scaleの追加

--- ChroMapper-CameraMovement/UserInterface/MainMenuUI.cs
+++ ChroMapper-CameraMovement/UserInterface/MainMenuUI.cs
@@ -33,7 +33,7 @@ namespace ChroMapper_CameraMovement.UserInterface
             _cameraMovementMainMenu.transform.parent = parent.transform;
 
             //Main Menu
-            UI.AttachTransform(_cameraMovementMainMenu, 170, 225, 1, 1, -50, -30, 1, 1);
+            UI.AttachTransform(_cameraMovementMainMenu, 170, 240, 1, 1, -50, -30, 1, 1);
 
             Image imageMain = _cameraMovementMainMenu.AddComponent<Image>();
             imageMain.sprite = PersistentUI.Instance.Sprites.Background;
@@ -53,66 +53,71 @@ namespace ChroMapper_CameraMovement.UserInterface
                 movementController.UiHidden();
                 UI.KeyDisableCheck();
             });
-            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Turn To Head", "Turn To Head", new Vector2(0, -70), Options.Instance.turnToHead, (check) =>
+            UI.AddCheckbox(_cameraMovementMainMenu.transform, "NJS Editor Scale", "NJS Editor Scale", new Vector2(0, -70), Settings.Instance.NoteJumpSpeedForEditorScale, (check) =>
+            {
+                Settings.Instance.NoteJumpSpeedForEditorScale = check;
+                Settings.ManuallyNotifySettingUpdatedEvent("NoteJumpSpeedForEditorScale", Settings.Instance.NoteJumpSpeedForEditorScale);
+            });
+            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Turn To Head", "Turn To Head", new Vector2(0, -85), Options.Instance.turnToHead, (check) =>
             {
                 Options.Instance.turnToHead = check;
                 movementController.Reload();
             });
-            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Avatar", "Avatar", new Vector2(0, -85), Options.Instance.avatar, (check) =>
+            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Avatar", "Avatar", new Vector2(0, -100), Options.Instance.avatar, (check) =>
             {
                 Options.Instance.avatar = check;
                 movementController.Reload();
             });
-            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Bookmark Lines", "Bookmark Lines", new Vector2(0, -100), Options.Instance.bookmarkLines, (check) =>
+            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Bookmark Lines", "Bookmark Lines", new Vector2(0, -115), Options.Instance.bookmarkLines, (check) =>
             {
                 Options.Instance.bookmarkLines = check;
                 movementController.Reload();
                 UI.KeyDisableCheck();
             });
-            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Sub Camera", "Sub Camera", new Vector2(0, -115), Options.Instance.subCamera, (check) =>
+            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Sub Camera", "Sub Camera", new Vector2(0, -130), Options.Instance.subCamera, (check) =>
             {
                 Options.Instance.subCamera = check;
                 movementController.Reload();
                 UI.KeyDisableCheck();
             });
-            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Bookmark Edit", "Bookmark Edit", new Vector2(0, -130), Options.Instance.bookmarkEdit, (check) =>
+            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Bookmark Edit", "Bookmark Edit", new Vector2(0, -145), Options.Instance.bookmarkEdit, (check) =>
             {
                 Options.Instance.bookmarkEdit = check;
                 UI._bookmarkMenuUI._cameraMovementBookmarkMenu.SetActive(check);
                 CameraControlPanelPosition();
                 UI.KeyDisableCheck();
             });
-            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Camera Control", "Camera Control", new Vector2(0, -145), Options.Instance.cameraControl, (check) =>
+            UI.AddCheckbox(_cameraMovementMainMenu.transform, "Camera Control", "Camera Control", new Vector2(0, -160), Options.Instance.cameraControl, (check) =>
             {
                 Options.Instance.cameraControl = check;
                 UI._cameraControlMenuUI._cameraControlMenu.SetActive(check);
                 CameraControlPanelPosition();
                 UI.KeyDisableCheck();
             });
-            var mainMenuMoreSettingsButton = UI.AddButton(_cameraMovementMainMenu.transform, "More Settings", "More Settings", new Vector2(0, -170), () =>
+            var mainMenuMoreSettingsButton = UI.AddButton(_cameraMovementMainMenu.transform, "More Settings", "More Settings", new Vector2(0, -185), () =>
             {
                 UI._settingMenuUI._cameraMovementSettingMenu.SetActive(true);
                 UI.KeyDisableCheck();
             });
-            UI.MoveTransform(mainMenuMoreSettingsButton.transform, 70, 25, 0.28f, 1, 0, -170);
+            UI.MoveTransform(mainMenuMoreSettingsButton.transform, 70, 25, 0.28f, 1, 0, -185);
 
-            var mainMenuReloadButton = UI.AddButton(_cameraMovementMainMenu.transform, "Reload", "Reload", new Vector2(0, -170), () =>
+            var mainMenuReloadButton = UI.AddButton(_cameraMovementMainMenu.transform, "Reload", "Reload", new Vector2(0, -185), () =>
             {
                 movementController.Reload();
             });
-            UI.MoveTransform(mainMenuReloadButton.transform, 70, 25, 0.72f, 1, 0, -170);
+            UI.MoveTransform(mainMenuReloadButton.transform, 70, 25, 0.72f, 1, 0, -185);
 
-            var mainMenuSettingSaveButton = UI.AddButton(_cameraMovementMainMenu.transform, "Setting Save", "Setting Save", new Vector2(0, -200), () =>
+            var mainMenuSettingSaveButton = UI.AddButton(_cameraMovementMainMenu.transform, "Setting Save", "Setting Save", new Vector2(0, -215), () =>
             {
                 Options.Instance.SettingSave();
             });
-            UI.MoveTransform(mainMenuSettingSaveButton.transform, 70, 25, 0.28f, 1, 0, -200);
+            UI.MoveTransform(mainMenuSettingSaveButton.transform, 70, 25, 0.28f, 1, 0, -215);
 
-            var mainMenuScriptMapperRunButton = UI.AddButton(_cameraMovementMainMenu.transform, "Script Mapper Run", "Script Mapper Run", new Vector2(0, -200), () =>
+            var mainMenuScriptMapperRunButton = UI.AddButton(_cameraMovementMainMenu.transform, "Script Mapper Run", "Script Mapper Run", new Vector2(0, -215), () =>
             {
                 ScriptMapperController.ScriptMapperRun(movementController);
             });
-            UI.MoveTransform(mainMenuScriptMapperRunButton.transform, 70, 25, 0.72f, 1, 0, -200);
+            UI.MoveTransform(mainMenuScriptMapperRunButton.transform, 70, 25, 0.72f, 1, 0, -215);
 
             _cameraMovementMainMenu.SetActive(false);
             UI._extensionBtn.Click = () =>

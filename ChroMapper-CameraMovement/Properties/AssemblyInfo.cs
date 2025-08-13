c0e6f5f2b788abe88506d144cd919ff3aa2a3a49
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2025-08-13 18:11:27 +0900

    VersionUP

--- ChroMapper-CameraMovement/Properties/AssemblyInfo.cs
+++ ChroMapper-CameraMovement/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ChroMapper-CameraMovement")]
-[assembly: AssemblyCopyright("Copyright ©  2024 rynan4818")]
+[assembly: AssemblyCopyright("Copyright ©  2025 rynan4818")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.11.4")]
-[assembly: AssemblyFileVersion("1.11.4")]
+[assembly: AssemblyVersion("1.11.5")]
+[assembly: AssemblyFileVersion("1.11.5")]
